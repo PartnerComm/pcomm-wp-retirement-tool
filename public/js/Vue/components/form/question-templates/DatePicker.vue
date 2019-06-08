a5b178175c6cbd8d63329b4d676dc072d7831138
Curt Bonade <cbonade@AdministorsMBP2.attlocal.net>
2019-06-07 20:00:18 -0400

    Add scaffold in datepicker component

---
+++ public/js/Vue/components/form/question-templates/DatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-datepicker">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {},
+  data() {
+    return {
+  
+    }
+  },
+  components: {
+
+  },
+  methods: {
+  
+  },
+  computed: {
+  
+  },
+  created() {
+
+  },
+  mounted() {
+
+  }
+  
+  }
+
+</script>
+
+<style>
+
+</style>
